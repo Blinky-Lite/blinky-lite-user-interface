28a2cbd69a66958694861d1a2a244ef2183d6249
David McGinnis <dmcginnis427@gmail.com>
2018-12-30 14:05:06 +0000

    first commit

---
+++ init-blinky-lite.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo su - pi -c "/usr/bin/screen -S devViewr -d -m /home/pi/deviceViewer/run-blinky-lite.sh"
+
---
+++ run-blinky-lite.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sleep 5
+cd /home/pi/deviceViewer/blinky-lite
+source runNodeRed.sh
+
+
